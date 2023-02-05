564358a9e4f40664000abf664b28c8ed77d41682
ryxn <sheppardryan@rogers.com>
2023-02-05 16:42:14 -0500

    feat: add authenticate()

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,18 @@
+import configparser
+import boto3
 
-def authenticate():
-    return "Unimplemented"
+
+def authenticate(
+        relative_config_location: str,
+        region: str = "us-east-2") -> object:
+    aws_config = configparser.ConfigParser()
+    aws_config.read(relative_config_location)
+    return boto3.client(
+        "dynamodb",
+        aws_access_key_id=aws_config.get("default", "aws_access_key_id"),
+        aws_secret_access_key=aws_config.get("default", "aws_secret_access_key"),
+        region_name=region
+    )
 
 
 def create_table():
