be145a1428328374145a441cdfe4c02389a62e89
ryxn <sheppardryan@rogers.com>
2023-02-06 20:29:37 -0500

    feat: add csv_to_json()

--- utils/parser.py
+++ utils/parser.py
@@ -1,4 +1,14 @@
+import csv
 
 
-def csv_to_json():
-    return "Unimplemented"
+def csv_to_dict(file_location: str) -> dict:
+    csv_file_rows = []
+
+    with open(file_location, encoding="utf-8") as csv_file:
+        csv_file_reader = csv.DictReader(csv_file)
+
+        for row in csv_file_reader:
+            csv_file_rows.append(row)
+
+    csv_file.close()
+    return csv_file_rows
